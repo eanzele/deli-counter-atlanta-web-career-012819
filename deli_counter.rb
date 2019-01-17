5d3f239816804e6d86b70fc8c5797fd6f6ebe72d
Emmanuel Anzele <eanzele@gmail.com>
2019-01-17 23:40:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,14 @@ def now_serving(katz_deli)
   end
 
 end
+
+def line(katz_deli)
+   if katz_deli.length == 0
+   puts "The line is currently empty."
+ else current_line = "The line is currently:"
+   katz_deli.each.with_index(1) do |customer, i|
+     current_line << " #{i}. #{customer}"
+   end
+   puts current_line
+ end
+ end
